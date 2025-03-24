b2afbda5eeca84bc3ba2b7fabe6c614facc50083
Christiaan Visagie <visagiechristiaan40@gmail.com>
2025-03-24 13:39:32 +0200

    Bank Card Module: - Create new card added - Api routes added

--- RESTapi/Modules/BankCards/Http/Controllers/BankCardsController.php
+++ RESTapi/Modules/BankCards/Http/Controllers/BankCardsController.php
@@ -4,9 +4,89 @@ namespace Modules\BankCards\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Modules\BankCards\Entities\BankCards;
+use Modules\BankCards\Entities\BankCardTypes;
 
 class BankCardsController extends Controller
 {
+
+    public function getAllUserCards($id){
+//        $userCards =
+    }
+
+    public function createNewCard(Request $request)
+    {
+        $user = $request->user();
+        $userId = $user->id;
+        $cardName = $request->get('bank_card_name');
+        $cardTypeSlug = request()->segment(count(request()->segments()));
+        $cardTypeSlugSingular = substr($cardTypeSlug, 0, -1);
+
+        $cardTypeID = BankCardTypes::where('slug', $cardTypeSlugSingular)->first()->id;
+
+        $cardDetails = $this->createCardDetails();
+
+        $cardNumber = $cardDetails['card_number'];
+        $cvv = $cardDetails['cvv'];
+        $expiryDate = $cardDetails['expiry_date'];
+
+        $newCard = BankCards::create([
+            'user_id' => $userId,
+            'bank_card_number' => $cardNumber,
+            'cvv' => $cvv,
+            'expiry_date' => $expiryDate,
+            'bank_card_type_id' => $cardTypeID,
+            'bank_card_name' => $cardName,
+        ]);
+
+        if($newCard){
+            return response()->json([
+                "status" => 201,
+                "message" => "{$cardName} successfully created",
+            ]);
+        }else{
+            return response()->json([
+                "status" => 501,
+                "message" => "{$cardName} could not be created",
+            ]);
+        }
+    }
+
+    public function createCardDetails () {
+        $cardDetails = [];
+        $cvv = "";
+
+        $dateDay = date('d');
+        $dateMonth = date('m');
+        $dateYear = date('y');
+
+        $firstSegment = $dateDay.$dateMonth;
+        $secondSegment = $dateYear.rand(0,9).rand(0,9);
+        $thirdSegment = "";
+        $forthSegment = "";
+
+        for($i = 0; $i <= 3; $i++) {
+            $thirdSegment .= rand(0,9);
+        }
+
+        for($i = 0; $i <= 3; $i++) {
+            $forthSegment .= rand(0,9);
+        }
+
+
+        for($i = 0; $i <= 2; $i++) {
+            $cvv .= rand(0,9);
+        }
+
+        $expiryDate = date("Y-m-d",strtotime("+ 4years"));
+
+        $cardDetails['card_number'] = $firstSegment.$secondSegment.$thirdSegment.$forthSegment;
+        $cardDetails['cvv'] = $cvv;
+        $cardDetails['expiry_date'] = $expiryDate;
+
+        return $cardDetails;
+
+    }
     /**
      * Display a listing of the resource.
      */
--- RESTapi/Modules/BankCards/routes/api.php
+++ RESTapi/Modules/BankCards/routes/api.php
@@ -4,5 +4,16 @@ use Illuminate\Support\Facades\Route;
 use Modules\BankCards\Http\Controllers\BankCardsController;
 
 Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
-    Route::apiResource('bankcards', BankCardsController::class)->names('bankcards');
+    // GET REQUESTS
+    Route::get('debit-cards/{id}', [BankCardsController::class, 'getAllUserCards'])->name('bankcards.getAllUserCards');
+//    Route::get('debit-cards/{id}', BankCardsController::class)->name('bankcards.getAllUserCards');
+
+    // POST REQUESTS
+    Route::post('debit-cards', [BankCardsController::class, 'createNewCard'])->name('bankcards.createNewCard');
+//
+//    // PUT REQUESTS
+//    Route::put('debit-cards/{id}', BankCardsController::class)->name('bankcards');
+//
+//    // DELETE REQUESTS
+//    Route::delete('debit-cards/{id}', BankCardsController::class)->name('bankcards');
 });
