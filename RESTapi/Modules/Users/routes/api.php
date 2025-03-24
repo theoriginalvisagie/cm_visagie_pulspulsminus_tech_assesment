761a80bfe502e246ad3a9bcc4a13da6f9a25cf0d
Christiaan Visagie <visagiechristiaan40@gmail.com>
2025-03-24 11:20:43 +0200

    Code Clean up

--- RESTapi/Modules/Users/routes/api.php
+++ RESTapi/Modules/Users/routes/api.php
@@ -3,11 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use Modules\Users\Http\Controllers\UsersController;
 
-Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
-    Route::apiResource('users', UsersController::class)->names('users');
-});
-
-//VerifyApiToken::class,
 Route::middleware(['throttle:60,1'])->group(function () {
     // POST REQUESTS
     ROUTE::post('login-user', [UsersController::class, 'loginUser']);
