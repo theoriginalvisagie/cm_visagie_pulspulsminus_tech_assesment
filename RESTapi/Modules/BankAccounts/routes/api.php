3b5c0d9d2c57ae34961a7816d64686058ffee9a5
Christiaan Visagie <visagiechristiaan40@gmail.com>
2025-03-24 19:29:39 +0200

    Routes update

--- RESTapi/Modules/BankAccounts/routes/api.php
+++ RESTapi/Modules/BankAccounts/routes/api.php
@@ -6,12 +6,11 @@ use Modules\BankAccounts\Http\Controllers\BankAccountsController;
 Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
     // GET REQUESTS
     //  List all bank accounts owned by the authenticated customer.
-    Route::get('accounts/', [BankAccountsController::class, 'getAllUserBankAccounts'])->name('bankcards.getAllUserBankAccounts');
+    Route::get('accounts/', [BankAccountsController::class, 'getAllUserBankAccounts'])->name('bankaccounts.getAllUserBankAccounts');
     //  List a bank account owned by the authenticated customer.
-    Route::get('accounts/{id}', [BankAccountsController::class, 'getBankAccountWithId'])->name('bankcards.getBankAccountWithId');
+    Route::get('accounts/{id}', [BankAccountsController::class, 'getBankAccountWithId'])->name('bankaccounts.getBankAccountWithId');
 
     // POST REQUESTS
     // Create a new bank account for the authenticated customer.
-    Route::post('accounts/', [BankAccountsController::class, 'createNewUserBankAccount'])->name('bankcards.createNewUserBankAccount');
-
+    Route::post('accounts/', [BankAccountsController::class, 'createNewUserBankAccount'])->name('bankaccounts.createNewUserBankAccount');
 });
